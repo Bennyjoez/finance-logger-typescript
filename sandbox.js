b1dfe21b3651bbd0c54bc82a76bc6dbfddf6550b
Benson <njugunab655@gmail.com>
2023-10-06 18:58:13 +0300

    First commit

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+// let character = 'Benson Njuguna'
+// let age = 30;
+// let isBlackBelt = false;
+// character = 'Kamau'
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// }
+// console.log(circ(10))
+// arrays
+var names = ["benson", "njuguna", "kamau"];
+names.push(2);
---
+++ sandbox.ts
@@ -0,0 +1,17 @@
+// let character = 'Benson Njuguna'
+// let age = 30;
+// let isBlackBelt = false;
+// character = 'Kamau'
+
+
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// }
+
+// console.log(circ(10))
+
+// arrays
+
+let names = ["benson", "njuguna", "kamau"];
+
+names.push(2)

23396ef3f776ee1eaeb631277d011b1fef998ef3
Benson <njugunab655@gmail.com>
2023-10-10 08:39:25 +0300

    Create new invoices and store in container

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,13 @@ const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
-const invOne = new Invoice("Benson", "Photos taken on event", 100);
-
 let invoices: Invoice[] = []
-invoices.push(invOne)
-console.log(invoices);
-console.log(invOne.client);
-
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
-  console.log(
-    type.value,
-    toFrom.value,
-    details.value,
-    amount.valueAsNumber
-  )
+
+  if (type.value == "invoice") {
+    const inv = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    invoices.push(inv);
+  }
 })

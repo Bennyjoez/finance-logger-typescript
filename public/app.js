1ff589537a3559b2a50f9a30e7e5baf47abeb23f
Benson <njugunab655@gmail.com>
2023-10-11 18:56:37 +0300

    Convert typescript

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ const type = document.querySelector("#type");
 const toFrom = document.querySelector("#toFrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const itemList = document.querySelector(".item-list");
 let invoices = [];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -11,4 +12,16 @@ form.addEventListener("submit", (e) => {
         const inv = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
         invoices.push(inv);
     }
+    display(invoices);
 });
+const display = (invoices) => {
+    invoices.map((inv) => {
+        const li = document.createElement('li');
+        const name = document.createElement("span");
+        const amount = document.createElement('span');
+        name.textContent = inv.client;
+        amount.textContent = `${inv.amount}`;
+        li.textContent = inv.client;
+        itemList === null || itemList === void 0 ? void 0 : itemList.appendChild(li);
+    });
+};

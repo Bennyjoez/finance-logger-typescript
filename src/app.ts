0448ef84522fa7258f8e008e1b845187cac6eb10
Benson <njugunab655@gmail.com>
2023-10-11 18:55:59 +0300

    Display invoice

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+const itemList = document.querySelector(".item-list")
 
 let invoices: Invoice[] = []
 
@@ -15,4 +16,18 @@ form.addEventListener("submit", (e: Event) => {
     const inv = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
     invoices.push(inv);
   }
+  display(invoices)
 })
+
+const display = (invoices: Invoice[]) => {
+  invoices.map((inv) => {
+    const li = document.createElement('li');
+    const name = document.createElement("span")
+    const amount = document.createElement('span')
+    name.textContent = inv.client
+    amount.textContent = `${inv.amount}`
+
+    li.textContent = inv.client
+    itemList?.appendChild(li);
+  })
+}

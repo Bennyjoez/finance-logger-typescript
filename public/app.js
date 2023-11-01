bca42b8be2db6d4460803cfd54c3d9410ad7805e
Benson <njugunab655@gmail.com>
2023-11-01 19:08:07 +0300

    Adjust adding of invoices

--- public/app.js
+++ public/app.js
@@ -6,32 +6,26 @@ const toFrom = document.querySelector("#toFrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 const itemList = document.querySelector(".item-list");
-let invoices = [];
-let payments = [];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (type.value == "invoice") {
         const inv = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
-        invoices.push(inv);
-        displayInvoices(invoices);
+        displayInvoices(inv);
     }
     else if (type.value == "payment") {
         const payment = new Payment(toFrom.value, details.value, amount.valueAsNumber);
-        // payments.push(payment)
         displayPayment(payment);
     }
 });
-const displayInvoices = (invoices) => {
-    invoices.map((inv) => {
-        const li = document.createElement('li');
-        const name = document.createElement("span");
-        const amount = document.createElement('span');
-        name.textContent = inv.client;
-        amount.textContent = `${inv.amount}`;
-        li.appendChild(name);
-        li.appendChild(amount);
-        itemList === null || itemList === void 0 ? void 0 : itemList.appendChild(li);
-    });
+const displayInvoices = (inv) => {
+    const li = document.createElement('li');
+    const name = document.createElement("span");
+    const amount = document.createElement('span');
+    name.textContent = inv.client;
+    amount.textContent = `${inv.amount}`;
+    li.appendChild(name);
+    li.appendChild(amount);
+    itemList === null || itemList === void 0 ? void 0 : itemList.appendChild(li);
 };
 const displayPayment = (payment) => {
     const li = document.createElement('li');
--- src/app.ts
+++ src/app.ts
@@ -6,37 +6,30 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
-const itemList = document.querySelector(".item-list")
-
-let invoices: Invoice[] = []
-let payments: Payment[] = []
+const itemList = document.querySelector(".item-list");
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
   if (type.value == "invoice") {
     const inv = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
-    invoices.push(inv);
-    displayInvoices(invoices)
+    displayInvoices(inv)
   } else if(type.value == "payment") {
     const payment = new Payment(toFrom.value, details.value, amount.valueAsNumber)
-    // payments.push(payment)
     displayPayment(payment)
   }
 })
 
-const displayInvoices = (invoices: Invoice[]) => {
-  invoices.map((inv) => {
-    const li = document.createElement('li');
-    const name = document.createElement("span")
-    const amount = document.createElement('span')
-    name.textContent = inv.client
-    amount.textContent = `${inv.amount}`
+const displayInvoices = (inv: Invoice) => {
+  const li = document.createElement('li');
+  const name = document.createElement("span")
+  const amount = document.createElement('span')
+  name.textContent = inv.client
+  amount.textContent = `${inv.amount}`
 
-    li.appendChild(name)
-    li.appendChild(amount)
-    itemList?.appendChild(li);
-  })
+  li.appendChild(name)
+  li.appendChild(amount)
+  itemList?.appendChild(li);
 }
 
 const displayPayment = (payment: Payment) => {

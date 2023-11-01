4c1368535609728f52861a1c4a10290a4d95df19
Benson <njugunab655@gmail.com>
2023-11-01 18:49:39 +0300

    Convert typescript

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/payments.js';
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#toFrom");
@@ -6,13 +7,19 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 const itemList = document.querySelector(".item-list");
 let invoices = [];
+let payments = [];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (type.value == "invoice") {
         const inv = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
         invoices.push(inv);
+        displayInvoices(invoices);
     }
-    displayInvoices(invoices);
+    else if (type.value == "payment") {
+        const payment = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        payments.push(payment);
+        displayPayments(payments);
+    }
 });
 const displayInvoices = (invoices) => {
     invoices.map((inv) => {
--- public/classes/payments.js
+++ public/classes/payments.js
@@ -1,4 +1,4 @@
-export class Payments {
+export class Payment {
     constructor(client, details, amount) {
         this.details = details;
         this.amount = amount;
